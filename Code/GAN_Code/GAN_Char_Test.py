98a4fd34b1b2de174c5337a73306015d913da61f
TMTR Creations <tmtr.creations@gmail.com>
2018-08-07 22:48:02 -0400

    Separated GAN code

--- Code/GAN_Code/GAN_Char_Test.py
+++ Code/GAN_Code/GAN_Char_Test.py
@@ -1,17 +1,36 @@
-from __future__ import print_function, division
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 2018
+
+Author: 
+Modified By: tmtrcreations
+"""
+
+# --------------------------
+# Import the needed modules
+# --------------------------
 
 from keras.layers import Input, Dense, Reshape, Flatten
 from keras.layers import BatchNormalization, Activation, AveragePooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
-from keras.optimizers import Adam
-
+from keras.optimizers import Adam, SGD
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
+# -----------
+#  GAN Class 
+# -----------
+
 class GAN():
+    
+    # --------------------
+    #  Define Initializer 
+    # --------------------
+    
     def __init__(self):
         self.img_rows = 40
         self.img_cols = 40
@@ -19,8 +38,8 @@ class GAN():
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
         self.latent_dim = 100
 
-        optimizer_d = Adam(0.000017, 0.5)
-        optimizer_g = Adam(0.0002, 0.5)
+        optimizer_d = SGD(0.001)
+        optimizer_g = Adam(0.002)
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
@@ -47,35 +66,29 @@ class GAN():
         self.combined = Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer_g)
 
+    # ------------------
+    #  Define Generator 
+    # ------------------
 
     def build_generator(self):
 
         model = Sequential()
         
         model.add(Dense(128 * 5 * 5, activation='relu', input_dim=self.latent_dim))
-        #model.add(LeakyReLU(alpha=0.2))
         model.add(Reshape((5, 5, 128)))
         model.add(UpSampling2D())
         
         model.add(Conv2D(64, kernel_size=2, strides=1, padding="same"))
         model.add(BatchNormalization(momentum=0.9))  
         model.add(Activation("relu"))
-        #model.add(LeakyReLU(alpha=0.2))
         model.add(UpSampling2D())
         
         model.add(Conv2D(32, kernel_size=2, strides=1, padding="same"))
         model.add(BatchNormalization(momentum=0.9))
         model.add(Activation("relu"))
-        #model.add(LeakyReLU(alpha=0.2))
         model.add(UpSampling2D())
         
         model.add(Conv2D(self.channels, kernel_size=2, strides=1, padding="same"))
-        
-        #model.add(BatchNormalization(momentum=0.9))
-        #model.add(LeakyReLU(alpha=0.2))
-        #model.add(UpSampling2D())
-        
-        #model.add(Conv2D(self.channels, kernel_size=3, strides=1, padding="same", activation='relu'))
         model.add(Activation("tanh"))
 
         model.summary()
@@ -84,6 +97,10 @@ class GAN():
         img = model(noise)
 
         return Model(noise, img)
+    
+    # ----------------------
+    #  Define Discriminator 
+    # ----------------------
 
     def build_discriminator(self):
 
@@ -93,35 +110,40 @@ class GAN():
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.9))
         model.add(AveragePooling2D((2, 2)))
+        
         model.add(Conv2D(64, (2, 2), padding='valid'))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.9))
         model.add(AveragePooling2D((2, 2)))
+        
         model.add(Conv2D(128, (2, 2), padding='valid'))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.9))
         model.add(AveragePooling2D((2, 2)))
+        
         model.add(Flatten())
         model.add(Dense(512))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(1, activation='sigmoid'))
+        
         model.summary()
 
         img = Input(shape=self.img_shape)
         validity = model(img)
 
         return Model(img, validity)
+    
+    # -----------------
+    #  Define Training 
+    # -----------------
 
     def train(self, epochs, batch_size=128, sample_interval=50):
 
         # Load the dataset
-        X_train = 255*np.ones((1722*2, self.img_rows, self.img_cols, self.channels))
-        for img_ind in range(1, 1723):
-            img = Image.open('../../Images/Training_Images/Single_Sprites/img' + str(img_ind) + '.png')
+        X_train = 255*np.ones((batch_size, self.img_rows, self.img_cols, self.channels))
+        for img_ind in range(1, batch_size+1):
+            img = Image.open('../../Images/Training_Images/Single_Sprites/img9.png')
             X_train[img_ind-1, 5:35, :, :] = np.array(img)
-        for img_ind in range(1, 1723):
-            img = Image.open('../../Images/Training_Images/Single_Sprites_Flipped/img_flipped' + str(img_ind) + '.png')
-            X_train[1722+img_ind-1, 5:35, :, :] = np.array(img)
 
         # Rescale -1 to 1
         X_train = X_train / 127.5 - 1.
@@ -137,8 +159,8 @@ class GAN():
             # ---------------------
 
             # Select a random batch of images
-            idx = np.random.randint(0, X_train.shape[0], batch_size)
-            imgs = X_train[idx]
+            imgs = X_train
+            imgs = imgs + (0.5*(np.cos(np.pi*epoch/60000)+1)/2)*np.random.normal(0, 1, imgs.shape)
 
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
 
@@ -166,6 +188,10 @@ class GAN():
             if epoch % sample_interval == 0:
                 self.sample_images(epoch)
 
+    # --------------------------
+    #  Define Sample Generation 
+    # --------------------------
+
     def sample_images(self, epoch):
         r, c = 5, 5
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
@@ -173,8 +199,6 @@ class GAN():
 
         # Rescale images 0 - 1
         gen_imgs = 0.5 * gen_imgs + 0.5
-        #gen_imgs[gen_imgs < 0] = 0
-        #gen_imgs[gen_imgs > 1] = 1
 
         fig, axs = plt.subplots(r, c)
         cnt = 0
@@ -186,7 +210,10 @@ class GAN():
         fig.savefig("../../Images/Generated_Sprites/%d.png" % epoch)
         plt.close()
 
-
+# ------
+#  Main
+# ------
+        
 if __name__ == '__main__':
     gan = GAN()
-    gan.train(epochs=60000, batch_size=128, sample_interval=200)
+    gan.train(epochs=60000, batch_size=32, sample_interval=200)
